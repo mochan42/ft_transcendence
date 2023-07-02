709011f84adbb92aae47e6830e484fa518723607
pmeising <philippmeisinger@googlemail.com>
2023-07-02 20:36:19 +0200

    Landing Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,18 @@ import LandingPage from './components/LandingPage'
 
 function App() {
 
-	const [signedInStatus, setSignedInStatus] = useState(false)
-	if (!signedInStatus)
+	// const [signedInStatus, setSignedInStatus] = useState(false)
+	// if (!signedInStatus)
+	// 	return (
+	// 		<div>
+	// 			<LandingPage/>
+	// 		</div>
+	// 	)
+	// if (signedInStatus)
 		return (
 			<div>
 				<LandingPage/>
-			</div>
-		)
-	if (signedInStatus)
-		return (
-			<div>
-				<h1 className="text-3xl font-bold underline text-white">
-				Hello world!
-				</h1>
-				<Navbar/>
+				{/* <Navbar/> */}
 			</div>
 		);
 }
