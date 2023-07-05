bb6a27a5661651e29823721ece8a06ac32e0acc8
pmeising <philippmeisinger@googlemail.com>
2023-07-05 23:11:18 +0200

    Made progress transitioning the .js code into Typescript and JSX element types. Integration is continuous. Will merge with dev_Philipp and continue there.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
+import React from 'react'
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/pages/Login';
 import Navbar from './components/Navbar';
-import LandingPage from './components/LandingPage'
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './components/pages/Home';
+import Profile from './components/pages/Profile';
+import LandingPage from './components/pages/LandingPage';
+import About from './components/pages/About'
 
-function App() {
+const App: React.FC = () => {
+	const [isAuth, setIsAuth] = useState(false)
 
-	// const [signedInStatus, setSignedInStatus] = useState(false)
-	// if (!signedInStatus)
-	// 	return (
-	// 		<div>
-	// 			<LandingPage/>
-	// 		</div>
-	// 	)
-	// if (signedInStatus)
 		return (
-			<div>
-				<LandingPage/>
-				{/* <Navbar/> */}
-			</div>
-		);
+			<Router>
+				<div>
+					<Navbar setIsAuth={setIsAuth} isAuth={isAuth}/>
+					<div className='h-screen bg-gray-200 w-full grid place-items-center'>
+						<Routes>
+							<Route path='/' element={<ProtectedRoute isAuth={isAuth} path='/' element={<Home />} />} />
+							<Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
+							{/* <Route path='game' element={<ProtectedRoute isAuth={isAuth} path='/game' element={<Game />} />} /> */}
+							<Route path='/profile' element={<ProtectedRoute isAuth={isAuth} path='/profile' element={<Profile />} />} />
+							<Route path='/landingpage' element={<ProtectedRoute isAuth={isAuth} path='/landingpage' element={<LandingPage />} />} />
+							<Route path='about' element={<About />} />
+						</Routes>
+					</div>
+				</div>
+			</Router>
+		)
 }
 
 export default App;
---
+++ frontend/src/components/pages/About.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom"
+import Button from "../ui/Button"
+
+const About = () => {
+	const navigate = useNavigate()
+	return (
+		<div className='h-screen bg-gray-200 w-full grid place-items-center'>
+			<h1>Transcendence Project of the 42 Core Curriculum</h1>
+			<p>The 42 Core Curriculum is a unique learning opportunity for everyone. Get involved and dive into the world of C programming. On this long
+				journey you'll make your way to the final boss of the first year: Transcendence ... 
+				Come on in and transcend with us onto new adventures!
+				Come on in and start adventuring!
+			</p>
+			<Button
+				className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow'
+				onClick={() => navigate('/login')}>
+				Login!
+			</Button>
+		</div>
+	)
+}
+
+export default About
