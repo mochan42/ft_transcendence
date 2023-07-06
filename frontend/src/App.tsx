ab50be02364d237d7dc6553c41bf370a1d4fd603
pmeising <philippmeisinger@googlemail.com>
2023-07-06 15:15:59 +0200

    Built the layout of the app.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,28 +10,58 @@ import Profile from './components/pages/Profile';
 import LandingPage from './components/pages/LandingPage';
 import About from './components/pages/About';
 import PageNotFound from './components/pages/PageNotFound';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
 	const [isAuth, setIsAuth] = useState(true)
 
 		return (
-			<Router>
-				<div className='flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8'>
-					<Navbar setIsAuth={setIsAuth} isAuth={isAuth}/>
-					<div className='h-screen bg-gray-200 w-full '>
-						<Routes>
-							<Route path='/' element={<ProtectedRoute isAuth={isAuth} path='/' element={<Home />} />} />
-							<Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
-							{/* <Route path='game' element={<ProtectedRoute isAuth={isAuth} path='/game' element={<Game />} />} /> */}
-							<Route path='/profile' element={<ProtectedRoute isAuth={isAuth} path='/profile' element={<Profile />} />} />
-							<Route path='/landingpage' element={<ProtectedRoute isAuth={isAuth} path='/landingpage' element={<LandingPage />} />} />
-							<Route path='about' element={<About />} />
-							<Route path='/*' element={<PageNotFound />}/>
-						</Routes>
+			<div className='grid grid-rows-8 gap-2'>
+				<Router>
+					<div className='shadow-xl flex backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 item-center justify-between'>
+						<Navbar setIsAuth={setIsAuth} isAuth={isAuth}/>
 					</div>
-				</div>
-			</Router>
+					<div className='bg-green-500 rounded-lg shadow-xl min-h-[50px]'>
+						<div>
+							<Routes>
+								<Route path='/' element={<ProtectedRoute isAuth={isAuth} path='/' element={<Home />} />} />
+								<Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
+								{/* <Route path='game' element={<ProtectedRoute isAuth={isAuth} path='/game' element={<Game />} />} /> */}
+								<Route path='/profile' element={<ProtectedRoute isAuth={isAuth} path='/profile' element={<Profile />} />} />
+								<Route path='/landingpage' element={<ProtectedRoute isAuth={isAuth} path='/landingpage' element={<LandingPage />} />} />
+								<Route path='about' element={<About />} />
+								<Route path='/*' element={<PageNotFound />}/>
+							</Routes>
+						</div>
+					</div>
+					<div className=' shadow-xl flex backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 items-center justify-evenly'>
+						<Footer/>
+					</div>
+				</Router>
+			</div>
 		)
 }
 
 export default App;
+
+
+/* CHAT-GPT:
+Inside each component directory (pages, components, etc.), create an index.ts file if it doesn't already exist.
+
+In the index.ts file, export the component from its corresponding file. For example, in components/index.ts:
+
+jsx
+Copy code
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+// Add other component exports as needed
+Now, you can import the components using a single line in your code:
+jsx
+Copy code
+import { Login, Navbar, ProtectedRoute, Home, Profile, LandingPage, About, PageNotFound, Footer } from './components';
+By using the index.ts file, you can group and export multiple components from the same directory in a single import statement. This makes your import statements cleaner and more concise.
+
+Make sure to adjust the file paths and directory names based on your project structure.
+
+Note: If you have nested directories within the components directory, you can create additional index.ts files in those directories as well to export the nested components.
+*/
