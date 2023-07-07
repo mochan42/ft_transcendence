2abe0d97bdd916bd48a490d7963078eff417a0b3
pmeising <philippmeisinger@googlemail.com>
2023-07-07 20:45:16 +0200

    Added APIS folder from the working version of Monine into the dev branch. Seems to be working now

--- apis/src/app.module.ts
+++ apis/src/app.module.ts
@@ -1,30 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
-import * as Joi from '@hapi/joi';
-import { ConfigModule } from '@nestjs/config';
-import { StatModule } from './stat/stat.module';
-import { PMatchModule } from './p_match/p_match.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        PORT: Joi.number(),
-      }),
-    }),
-    DatabaseModule,
-    StatModule,
-    PMatchModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
