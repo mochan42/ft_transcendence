c0b384e7664a20fe3d8d3b2612ca211491786805
pmeising <philippmeisinger@googlemail.com>
2023-07-20 11:24:42 +0200

    Added a reset button, which resets scores and restarts the game.

--- frontend/src/components/pages/Game.tsx
+++ frontend/src/components/pages/Game.tsx
@@ -41,6 +41,11 @@ const Game = () => {
 							<path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5V18M15 7.5V18M3 16.811V8.69c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 010 1.954l-7.108 4.061A1.125 1.125 0 013 16.811z" />
 						</svg>
 					</Button>
+					<Button variant='ghost' onClick={() => {setPlayerScore(0), setBotScore(0)}}>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3" />
+					</svg>
+					</Button>
 				</div>
 				<div className='border-8 dark:border-slate-900'>
 					<Button onClick={() => botPoint()}>
