2d691fd765b81af72c4a3d5da189d5466c477829
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2023-07-22 01:00:27 +0200

    #Cors error fixed, client can now call api

--- apis/src/app.controller.ts
+++ apis/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('pong/apis')
+@Controller('pong')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
