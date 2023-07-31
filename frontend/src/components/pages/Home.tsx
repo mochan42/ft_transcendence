d706ecf712729d336e3ced88d16e3c2a5ad17aa7
pmeising <philippmeisinger@googlemail.com>
2023-07-31 19:50:57 +0200

    Commented out a part on the Home page. Cudoh you might wanna double-check if that is neccessary for your logic here. Just to make it work I commented it out for now.

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -36,26 +36,26 @@ const Home = ({ userCode, loginState, userId }: TUserState) => {
         }
     }
 
-<!--     useEffect(() => {
-            const urlBrowser = window.location.href;
-            // parse the url and retrieve the query parameters
-            const urlSearchParams = new URLSearchParams(urlBrowser.split('?')[1]);
-            console.log(urlSearchParams);
-            Array.from((urlSearchParams.entries())).map(([key, value]) => {
-                if (key === "code") {
-                    userCode.setCode(value);
-                    loginState.setIsLogin(true);
-                    // send received code to the backend for further verification
-                    window.history.pushState({}, '', "http://localhost:3000/profile");
-                }
-                else { navigate('/about') }
-            })
-        }
-        , [userCode.code, loginState.isLogin]);
+    //  useEffect(() => {
+    //         const urlBrowser = window.location.href;
+    //         // parse the url and retrieve the query parameters
+    //         const urlSearchParams = new URLSearchParams(urlBrowser.split('?')[1]);
+    //         console.log(urlSearchParams);
+    //         Array.from((urlSearchParams.entries())).map(([key, value]) => {
+    //             if (key === "code") {
+    //                 userCode.setCode(value);
+    //                 loginState.setIsLogin(true);
+    //                 // send received code to the backend for further verification
+    //                 window.history.pushState({}, '', "http://localhost:3000/profile");
+    //             }
+    //             else { navigate('/about') }
+    //         })
+    //     }
+    //     , [userCode.code, loginState.isLogin]);
     
-    if (userCode.code) {
-        authenticateToAPI(userCode.code);
-    } -->
+    // if (userCode.code) {
+    //     authenticateToAPI(userCode.code);
+    // }
 
 	const getUsersInfo = async () => {
 		try {
