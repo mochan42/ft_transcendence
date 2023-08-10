e97d98a54fc9b2836fce0d3a0a9983067cb4f8a4
pmeising <philippmeisinger@googlemail.com>
2023-08-10 21:49:48 +0200

    additions

--- frontend/src/components/ui/Modal.tsx
+++ frontend/src/components/ui/Modal.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Button } from './Button'
 
 function Modal() {
     return (
         <div>
-            Modal is shown
+            <div>
+                <Button>
+                    Close
+                </Button>
+                <div>
+                    <h1>Title</h1>
+                </div>
+                <div>
+                    <p>Body</p>
+                </div>
+                <div>
+                    <Button>
+                        Cancel
+                    </Button>
+                    <Button>
+                        Continue
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
