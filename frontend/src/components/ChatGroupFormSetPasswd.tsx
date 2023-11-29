700b24068c76b12f854f9ed20a90d640b9885657
=Facinet Kouyate <=fakouyat@student.42wolfsburg.de>
2023-11-29 13:11:55 +0100

    fix: set passwd for private group

--- frontend/src/components/ChatGroupFormSetPasswd.tsx
+++ frontend/src/components/ChatGroupFormSetPasswd.tsx
@@ -5,9 +5,9 @@ import { Dialog, Slide, DialogTitle, DialogContent, RadioGroup, FormLabel } from
 import { TransitionProps } from '@mui/material/transitions';
 import * as Yup from 'yup';
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, Stack, Radio } from "@mui/material";  
-import Checkbox from "@mui/material/Checkbox";  
-import FormControlLabel from "@mui/material/FormControlLabel";  
+import { Button, Stack, Radio } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import RHF_TextField from './ui/RHF_TextField';
 import RHF_AutoCompDropDown from './ui/RHF_AutoCompDropDown';
 import Cookies from 'js-cookie';
@@ -23,12 +23,11 @@ import { updateChatDialogSetPasswd } from '../redux/slices/chatDialogSlice';
  * See vide0 12 form creation
  * See video 9 for custom textfield creation
  */
-const Transition = React.forwardRef(function Transition (
-    props: TransitionProps & { children: React.ReactElement<any, any>;},
-    ref: React.Ref<unknown>)
-    {
-        return <Slide direction="up" ref={ref} {...props} />;
-    }
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & { children: React.ReactElement<any, any>; },
+    ref: React.Ref<unknown>) {
+    return <Slide direction="up" ref={ref} {...props} />;
+}
 );
 
 
@@ -37,9 +36,9 @@ const CreateGroupFormSetPasswd = () => {
     const chatDialogStore = useSelector(selectChatDialogStore)
     const dispatch = useDispatch()
     const open = chatDialogStore.chatDialogSetPasswd
-    const handleClose = () => { 
+    const handleClose = () => {
         dispatch(updateChatDialogSetPasswd(false))
-    } 
+    }
 
 
     const groupSchema = Yup.object().shape(
@@ -49,14 +48,14 @@ const CreateGroupFormSetPasswd = () => {
         }
     )
 
-    const defaultValues = { 
-        passwd: "" ,
+    const defaultValues = {
+        passwd: "",
     }
 
     const methods = useForm({
         resolver: yupResolver(groupSchema),
         defaultValues,
-    }) 
+    })
 
     const {
         reset,
@@ -74,14 +73,13 @@ const CreateGroupFormSetPasswd = () => {
     //const onSubmit:SubmitHandler<TFormInputs> = async (data: TFormInputs) => {
     // would be easier if data has the same names with channels colums in apis side
     const onSubmit = async (data: any) => {
-        try{
+        try {
             let newGroupData = chatStore.chatActiveGroup;
 
             // set new password to new group data
-            if (newGroupData)
-            {
+            if (newGroupData) {
                 newGroupData = { ...newGroupData, password: getValues("passwd") }
-                if (newGroupData.privacy == enChatPrivacy.PUBLIC)
+                if (newGroupData.privacy != enChatPrivacy.PROTECTED)
                     newGroupData.privacy = enChatPrivacy.PROTECTED
                 if (newGroupData.password == "")
                     newGroupData.privacy = enChatPrivacy.PUBLIC
@@ -92,30 +90,29 @@ const CreateGroupFormSetPasswd = () => {
             })
             handleClose();
         }
-        catch (error)
-        {
+        catch (error) {
             console.log("EEROR!", error);
         }
         handleClose();
     }
 
     return (
-        <FormProvider {...methods} > 
+        <FormProvider {...methods} >
             <form onSubmit={methods.handleSubmit(onSubmit)}>
                 <Stack spacing={3} padding={2}>
-                    <RHF_TextField name="passwd" 
-                        label="Password" 
-                        type="password" 
+                    <RHF_TextField name="passwd"
+                        label="Password"
+                        type="password"
                     />
                 </Stack>
-                <Stack spacing={2} direction={"row"} 
+                <Stack spacing={2} direction={"row"}
                     alignItems={"center"}
                     justifyContent={"end"}
                 >
-                    <Button onClick={()=>handleClose()}>Cancel </Button>
+                    <Button onClick={() => handleClose()}>Cancel </Button>
                     <Button type="submit" variant='contained'>Set Password</Button>
                 </Stack>
-           </form>
+            </form>
         </FormProvider>
     )
 }
@@ -124,10 +121,10 @@ const ChatGroupFormSetPasswd = () => {
     const chatDialogStore = useSelector(selectChatDialogStore)
     const open = chatDialogStore.chatDialogSetPasswd
     return (
-        <Dialog fullWidth maxWidth="xs" 
+        <Dialog fullWidth maxWidth="xs"
             open={open} TransitionComponent={Transition}
             keepMounted
-            sx={{p: 4}}
+            sx={{ p: 4 }}
         >
             {/* Title */}
             <DialogTitle>Set Password</DialogTitle>
