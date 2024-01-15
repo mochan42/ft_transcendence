b72b0b29479f4cd862583e40e79dc962d725dabc
Philipp Meisinger <philipp.meisinger@8tronix.de>
2024-01-15 21:07:47 +0100

    Intermediate commit

--- frontend/src/data/Global.tsx
+++ frontend/src/data/Global.tsx
@@ -13,8 +13,8 @@
 
 //-------------- FACINET-------------------------------------------------------------------
 export const BACKEND_URL = "http://localhost:5000";
-export const FRONTEND_URL = "http://localhost:3000"
-export const REDIRECT_URI = "http%3A%2F%2Flocalhost%3A3000%2F"
+export const FRONTEND_URL = "http://localhost:3000";
+export const REDIRECT_URI = "http%3A%2F%2Flocalhost%3A3000%2F";
 
 // //-------------- MONINE-------------------------------------------------------------------
 // export const BACKEND_URL = "https://supreme-fishstick-45q79xggv9g37rwr-5000.app.github.dev";
